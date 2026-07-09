c0d0db23b541a67deb3e911ad84912d3945b20c8
riazi1game <matin.mos99@gmail.com>
2026-07-10 02:02:21 +0330

    Create levels.js

---
+++ levels.js
@@ -0,0 +1,63 @@
+/*==============================
+ Riazi1Game Levels v1.0
+==============================*/
+
+const games = [
+
+{
+id:1,
+title:"تشخیص عدد با انگشتان",
+icon:"✋",
+page:"games/fingers.html",
+locked:false
+},
+
+{
+id:2,
+title:"بشمار و بگو",
+icon:"🔢",
+page:"games/count.html",
+locked:true
+},
+
+{
+id:3,
+title:"رنگ کن",
+icon:"🎨",
+page:"games/color.html",
+locked:true
+},
+
+{
+id:4,
+title:"الگوها",
+icon:"🧩",
+page:"games/pattern.html",
+locked:true
+},
+
+{
+id:5,
+title:"وصل کن",
+icon:"🔗",
+page:"games/connect.html",
+locked:true
+},
+
+{
+id:6,
+title:"بیشتر یا کمتر",
+icon:"⚖️",
+page:"games/compare.html",
+locked:true
+},
+
+{
+id:7,
+title:"آزمون فصل",
+icon:"🏆",
+page:"games/exam.html",
+locked:true
+}
+
+];
